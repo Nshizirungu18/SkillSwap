84b302fd40b0d8084aefdf084af224d6973441c7
Nshizirungu18 <enshizirungu18@gmail.com>
2025-09-02 02:35:12 +0200

    Create config.py

---
+++ backend/app/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
