e642a7271c78738be0b30d6303ccd30ca9d8b4ae
Nshizirungu18 <enshizirungu18@gmail.com>
2025-09-02 11:10:32 +0200

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        "mysql+pymysql://user:password@localhost/skillswap_db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
